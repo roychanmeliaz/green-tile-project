9428d6c76ef61bdeec612c9b93521d47996a9810
roychanmeliaz <roychan.meliaz@gmail.com>
2023-11-27 11:46:51 +0700

    Painting tile at Mon 27 Nov 2023 11:46:51 AM WIB

--- painter.sh
+++ painter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /root/project/green-tile-project/
 echo 'Tile at location'  `date` 'painted' >> report.txt
+git pull
 git add .
 git commit -m "Painting tile at `date`"
 git push origin main
