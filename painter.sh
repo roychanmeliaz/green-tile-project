8d8fed6745eb27c30f6a27203272ac7466616522
roychanmeliaz <roychan.meliaz@gmail.com>
2024-01-14 21:30:05 +0700

    Painting tile at Sun 14 Jan 21:30:05 WIB 2024

--- painter.sh
+++ painter.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-cd ~/2024/green-tile-project/
-echo 'Tile at location'  `date` 'painted' >> report.txt
-git pull
-git add .
-git commit -m "Painting tile at `date`"
-git push origin main
+
+# Function to generate a random number based on the distribution
+generate_random_number() {
+    rand=$((RANDOM % 100))
+
+    if ((rand < 20)); then
+        echo 0
+    elif ((rand < 70)); then
+        echo $((1 + RANDOM % 3))
+    else
+        echo $((4 + RANDOM % 9))
+    fi
+}
+
+# Loop 10 times
+for ((i=0; i<10; i++)); do
+    random_number=$(generate_random_number)
+    
+    # Your script with the random number
+    cd ~/2024/green-tile-project/
+    echo "Tile at location" $(date) "painted with random number: $random_number" >> report.txt
+    git pull
+    git add .
+    git commit -m "Painting tile at $(date)"
+    git push origin main
+
+    # Sleep for a while (optional, to space out the iterations)
+    sleep 1
+done
